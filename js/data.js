58476980223277ae5cd98d57aba25fc541899531
urwatulwutsko766-code <urwatulwutsko766@gmail.com>
2026-09-14 00:55:42 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,8 @@
+window.RuangBacaUIBooks = [
+ {title:"Atomic Habits",author:"James Clear",className:"atomic",status:"reading",favorite:true},
+ {title:"The Intelligent Investor",author:"Benjamin Graham",className:"invest",status:"reading",favorite:true},
+ {title:"The Psychology of Money",author:"Morgan Housel",className:"money",status:"done",favorite:false},
+ {title:"Rich Dad Poor Dad",author:"Robert Kiyosaki",className:"rich",status:"done",favorite:true},
+ {title:"The Almanack of Naval Ravikant",author:"Eric Jorgenson",className:"atomic",status:"reading",favorite:false},
+ {title:"Think and Grow Rich",author:"Napoleon Hill",className:"invest",status:"done",favorite:false}
+];
